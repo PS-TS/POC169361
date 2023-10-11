e6028f38ac487aa443d93e8d5055bb1e21715fb1
PS-TS <115213337+PS-TS@users.noreply.github.com>
2023-10-11 10:34:29 +0200

    Hello World (test do nadpisania)

---
+++ lab2/zad1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static int suma(int a, int b){
+        return a+b;
+    }
+}
