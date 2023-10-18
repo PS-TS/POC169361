90e3018506339c954596835aa7353d5671b616c9
PS-TS <115213337+PS-TS@users.noreply.github.com>
2023-10-18 10:10:50 +0200

    zadanie na aktywność

--- lab3/zadanie 305/src/Main.java
+++ lab3/zadanie 305/src/Main.java
@@ -2,6 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println(maxZDwoch(10,5));
+        System.out.println(maxZDwoch(10,15));
+    }
 
+    public static int maxZDwoch(int a, int b){
+        if(a>b)
+            return a;
+        return b;
     }
 }
